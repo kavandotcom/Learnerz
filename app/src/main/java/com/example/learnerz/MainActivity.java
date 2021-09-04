3e2eece993f113018b4ddcd51a18306f5007e5ad
Faizan Ahmed Khan R K <faizanahmedkhan005@gmail.com>
2021-09-04 23:29:50 +0530

    botton to profile

--- app/src/main/java/com/example/learnerz/MainActivity.java
+++ app/src/main/java/com/example/learnerz/MainActivity.java
@@ -4,25 +4,34 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     FirebaseAuth auth;
 
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         auth = FirebaseAuth.getInstance();
+
 
     }
 
@@ -42,5 +51,10 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
             finish();
         }
+    }
+
+    public void profile(View view) {
+        Intent intent =new Intent(MainActivity.this,profile.class);
+        startActivity(intent);
     }
 }
