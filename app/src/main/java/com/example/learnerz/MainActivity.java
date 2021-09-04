1f863064847cb6535736f5b0f1177b52a814e1c0
Faizan Ahmed Khan R K <faizanahmedkhan005@gmail.com>
2021-09-05 01:23:14 +0530

    list view and toast message

--- app/src/main/java/com/example/learnerz/MainActivity.java
+++ app/src/main/java/com/example/learnerz/MainActivity.java
@@ -9,9 +9,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SearchView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
@@ -22,7 +25,10 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     FirebaseAuth auth;
-
+    SearchView mysearchView;
+    ListView myList;
+    ArrayList<String> list;
+    ArrayAdapter<String> adapter;
 
     @SuppressLint("WrongViewCast")
     @Override
@@ -32,6 +38,36 @@ public class MainActivity extends AppCompatActivity {
 
         auth = FirebaseAuth.getInstance();
 
+        mysearchView =(SearchView)findViewById(R.id.searchView);
+        myList = (ListView)findViewById(R.id.myList);
+        list = new ArrayList<String>();
+        list.add("Android Development");
+        list.add("Ethical Hacking");
+        list.add("Data Science");
+        list.add("Machine Learning And Artificial Intelligence");
+        list.add("Computer Graphics And Visualization");
+        list.add("Databases");
+        list.add("Computer Networking");
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,list);
+        myList.setAdapter(adapter);
+        myList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this,"Link Not available",Toast.LENGTH_SHORT).show();
+            }
+        });
+        mysearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                adapter.getFilter().filter(s);
+                return false;
+            }
+        });
 
     }
 
