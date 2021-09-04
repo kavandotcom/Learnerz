3cb6978e9c5664361aed1bf45deb23cecb2a1854
kavan <kavandotcom>
2021-09-04 22:53:53 +0530

    Learnerz App

--- app/src/main/java/com/example/learnerz/profile.java
+++ app/src/main/java/com/example/learnerz/profile.java
@@ -2,13 +2,26 @@ package com.example.learnerz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class profile extends AppCompatActivity {
+
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        auth = FirebaseAuth.getInstance();
+    }
+
+    public void logout(View view){
+        auth.signOut();
+        Intent intent = new Intent(profile.this , LoginActivity.class);
+        startActivity(intent);
     }
 }
