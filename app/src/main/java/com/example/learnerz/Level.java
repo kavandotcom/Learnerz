3c40dfe0d5bcc548508382b45bfc1ab57cdb693d
kavan <kavandotcom>
2021-09-04 20:50:21 +0530

    Learnerz App

---
+++ app/src/main/java/com/example/learnerz/Level.java
@@ -0,0 +1,15 @@
+package com.example.learnerz;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Level extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level);
+    }
+
+
+}
